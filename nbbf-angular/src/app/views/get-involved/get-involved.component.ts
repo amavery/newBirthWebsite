f2d2c31b2d85683fc5166db07ada9798aa250320
Jordan Maness (jomaness) <jomanessdev@gmail.com>
2017-08-11 21:42:10 -0400

    added video to get-involved

--- nbbf-angular/src/app/views/get-involved/get-involved.component.ts
+++ nbbf-angular/src/app/views/get-involved/get-involved.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { VideoBackground as VideoBackgroundRenderer } from '@squarespace/video-background-rendering';
+
+
+
 
 @Component({
   selector: 'app-get-involved',
@@ -10,6 +14,11 @@ export class GetInvolvedComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const config = {
+      container: document.documentElement.querySelector('.sqs-video-background'),
+      url: 'https://www.youtube.com/watch?v=ZyS6twkhXF8&feature=youtu.be'
+    };
+    const myVideoBackground = new VideoBackgroundRenderer(config);
   }
 
 }
