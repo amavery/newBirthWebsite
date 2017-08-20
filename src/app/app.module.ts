04acb3db641562bad8da4dcf4b4b9e3b70422e53
jomanessdev <jomanessdev@gmail.com>
2017-08-20 00:34:10 -0400

    media pics added & footer social media links updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MdDialogModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { SwiperModule } from 'angular2-useful-swiper'; //or for angular-cli the path will be ../../node_modules/angular2-useful-swiper 
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -13,6 +18,7 @@ import { GetInvolvedComponent } from './views/get-involved/get-involved.componen
 import { MediaComponent } from './views/media/media.component';
 
 import { VideoBackground } from '@squarespace/video-background';
+import { MediaDialogComponent } from './components/media-dialog/media-dialog.component';
 
 
 @NgModule({
@@ -22,15 +28,20 @@ import { VideoBackground } from '@squarespace/video-background';
     PageNotFoundComponent,
     WhatToExpectComponent,
     GetInvolvedComponent,
-    MediaComponent
+    MediaComponent,
+    MediaDialogComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     AppRoutingModule,
+    MdDialogModule,
+    BrowserAnimationsModule,
+    SwiperModule
   ],
   providers: [],
+  entryComponents: [MediaDialogComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
